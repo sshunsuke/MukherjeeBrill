6ead28de7b81f1228a5bc96ea9d5d78635cd88c8
shunsuke <s_shunske@yahoo.co.jp>
2021-03-31 17:42:27 +0800

    Renamed test-dff.R to test-friction_factor.R and added a new test file (test-dPdL.R).

---
+++ tests/testthat/test-dPdL.R
@@ -0,0 +1,13 @@
+
+
+
+test_that("multiplication works", {
+  
+  #dlns <- l_dlns_MB(vsG, vsL, D, densityG, densityL,
+  #                 viscosityG, viscosityL, surfaceTension, angle)
+  #fr   <- l_flow_regime_MB(dlns)
+  #hl <- l_holdup_MB(dlns, fr)
+  #dPdL <- l_dPdL_MB(dlns, fr, hl, roughness, pressure, debug=FALSE)
+  
+  expect_equal(2 * 2, 4)
+})
